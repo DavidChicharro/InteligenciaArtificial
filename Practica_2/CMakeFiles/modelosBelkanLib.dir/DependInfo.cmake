155118ed44020af8b6028162547a1c94eadd76ab
David <david.c.chicharro@gmail.com>
2019-06-27 17:02:21 +0200

    Add files via upload

---
+++ Practica_2/CMakeFiles/modelosBelkanLib.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/aldeano/aldeano.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/aldeano.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/aldeano/brazo.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/brazo.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/aldeano/cabeza.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/cabeza.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/aldeano/cintura.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/cintura.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/aldeano/pierna.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/pierna.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/aldeano/torso.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/torso.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/arbol/arbol.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/arbol/arbol.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/item/item.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/item/item.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/jugador/jugador3d.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/jugador/jugador3d.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/jugador/quesito.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/jugador/quesito.cpp.o"
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/src/modelos/retrato.cpp" "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/modelosBelkanLib.dir/src/modelos/retrato.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/david/Uni/IA/Practicas/Practica_2/Practica2/CMakeFiles/obj3DLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
